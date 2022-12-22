33e8f301bbfe33dd5bffd0272e81268508f34dbd
zzxn <zhuxn@shihe.tech>
2022-12-22 16:34:24 +0800

    update

---
+++ ftplugin/tex.vim
@@ -0,0 +1,7 @@
+" On pressing tab, insert 4 spaces
+setlocal expandtab
+" show existing tab with 4 spaces width
+setlocal tabstop=2
+setlocal softtabstop=2
+" when indenting with '>', use 4 spaces width
+setlocal shiftwidth=2
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -47,7 +47,9 @@ return require('packer').startup(function()
     }
     use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
     use {
-      'stevearc/aerial.nvim',
-      config = function() require('aerial').setup() end
+      'stevearc/aerial.nvim'
+    }
+    use {
+        'dhruvasagar/vim-table-mode'
     }
 end)
