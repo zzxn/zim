caa262885aaca34b8f2523fe23dae295bc59eab3
zzxn <zzxnhackman@foxmail.com>
2023-02-22 17:09:10 +0800

    add nvim-surround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -52,4 +52,13 @@ return require('packer').startup(function()
     use {
         'dhruvasagar/vim-table-mode'
     }
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
 end)
