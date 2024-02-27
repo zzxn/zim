e57686a25758e2445904eb21e631f7afc078f010
zzxn <zzxnhackman@foxmail.com>
2024-02-27 14:25:00 +0800

    feat: watch change files and support rust

--- lua/plugin-config/lspconfig.lua
+++ lua/plugin-config/lspconfig.lua
@@ -18,6 +18,13 @@ require("mason-lspconfig").setup {
 -- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+capabilities.workspace = {
+    {
+        didChangeWatchedFiles = {
+            dynamicRegistration = true
+        }
+    }
+}
 
 -- Golang
 lspconfig.golangci_lint_ls.setup {
@@ -86,6 +93,11 @@ lspconfig.lua_ls.setup {
             },
         },
     },
+}
+
+-- Rust
+lspconfig.rust_analyzer.setup {
+    capabilities = capabilities,
 }
 
 -- Global mappings.
